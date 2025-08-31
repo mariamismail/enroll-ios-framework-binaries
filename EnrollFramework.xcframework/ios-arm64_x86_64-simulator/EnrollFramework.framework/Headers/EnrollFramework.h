eb74f5acafe1648ef7ae01d04c070a817f3177e5
mariamismail <mariam.ismail@luminsoft.net>
2025-08-31 15:49:47 +0300

    Release 1.2.0

---
+++ EnrollFramework.xcframework/ios-arm64_x86_64-simulator/EnrollFramework.framework/Headers/EnrollFramework.h
@@ -0,0 +1,19 @@
+//
+//  EnrollFramework.h
+//  EnrollFramework
+//
+//  Created by Bahi El Feky on 30/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EnrollFramework.
+FOUNDATION_EXPORT double EnrollFrameworkVersionNumber;
+
+//! Project version string for EnrollFramework.
+FOUNDATION_EXPORT const unsigned char EnrollFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnrollFramework/PublicHeader.h>
+
+const char* getKey(void);
+const char* getFirebaseKey(void);
